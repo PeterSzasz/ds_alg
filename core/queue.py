6d40b15d6aeda520226222e0c544996c0a20d994
Péter Szász <PeterSzasz@users.noreply.github.com>
2021-02-03 19:51:01 +0100

    queue, stack refactored

--- core/queue.py
+++ core/queue.py
@@ -1,22 +1,36 @@
 # simple double-ended queue
 
 class queue:
-  '''simple queue with enqueue(enq), dequeue(deq), peek'''
+    '''simple queue with enqueue, dequeue, peek'''
 
-  def __init__(self) -> None:
-    self.dataplates = []
-    self.first = 0  # at the front, deq side
-    self.last = 0   # at the back, enq side
+    def __init__(self) -> None:
+        self.data = []
+        self.first = 0  # at the front, deq side
+        self.last = 0   # at the back, enq side
 
-  def enqueue(self, element) -> None:
-    self.dataplates.append(element)
-    self.last += 1
+    def enqueue(self, element) -> None:
+        self.data.append(element)
+        self.last += 1
 
-  def dequeue(self):
-    front_element = self.dataplates[self.first]
-    self.dataplates.remove(self.first)
-    self.first += 1
-    return front_element
+    def dequeue(self):
+        front_element = self.data.pop(self.first)
+        return front_element
 
-  def peek(self):
-    return self.dataplates[self.first]
+    def peek(self):
+        return self.data[self.first]
+
+    def size(self):
+        return len(self.data)
+
+    def empty(self):
+        return len(self.data) == 0
+
+if __name__ == "__main__":
+    q = queue()
+    for i in range(0, 9):
+        q.enqueue(i)
+    print(q.data)
+    for _ in range(q.size()):
+        print(q.dequeue())
+
+    print("queue empty" if q.empty() else "not empty")
--- core/stack.py
+++ core/stack.py
@@ -1,24 +1,54 @@
 # simple stack module
 
+class list_element:
+    '''a singly linked list like object'''
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+
 class stack:
-  '''slep.. sslomip... simple stack, with push n pop n peek'''
+    '''simple stack, with push n pop n peek'''
 
-  def __init__(self):
-    self.dataplates = []
-    self.size = 0
+    def __init__(self):
+        self.head = list_element("head")
+        self.size = 0
 
-  def pop(self):
-    last_element = self.dataplates[self.size-1]
-    self.size -= 1
-    self.dataplates.remove[self.size-1]
-    return last_element
+    def push(self, data):
+        element = list_element(data)
+        element.next = self.head.next
+        self.head.next = element
+        self.size += 1
 
-  def push(self, element):
-    self.dataplates.append(element)
-    self.size += 1
+    def pop(self):
+        if self.empty():
+            raise Exception("Stack is empty.")
+        last_element = self.head.next
+        self.head.next = self.head.next.next
+        self.size -= 1
+        return last_element.data
 
-  def peek(self):
-    return self.dataplates[self.size]
+    def peek(self):
+        if self.empty():
+            raise Exception("Stack is empty.")
+        return self.head.next.data
 
-  def size(self):
-    return self.size
+    def size(self):
+        return self.size
+
+    def empty(self):
+        return self.size == 0
+
+
+if __name__ == "__main__":
+    s = stack()
+    print("load")
+    for i in range(10):
+        s.push(i)
+        print(s.peek(), end=" ")
+
+    print("\nunload")
+    for i in range(10):
+        print(s.pop(), end=" ")
+
+    print("stack empty" if s.empty() else "not empty")
